72e2ae67d93843a30e50a712602b32a95db35f23
peterjanas <pejanas@gmail.com>
2024-10-22 12:58:14 +0200

    route

--- src/main/java/dat/routes/IngredientRoute.java
+++ src/main/java/dat/routes/IngredientRoute.java
@@ -1,5 +1,28 @@
 package dat.routes;
 
-public class IngredientRoute
-{
+
+import dat.controllers.impl.Ingredientcontroller;
+import dat.security.enums.Role;
+import io.javalin.apibuilder.EndpointGroup;
+
+import static io.javalin.apibuilder.ApiBuilder.*;
+
+public class IngredientRoute {
+
+    private final Ingredientcontroller ingredientcontroller = new Ingredientcontroller();
+
+    protected EndpointGroup getRoutes() {
+/*
+        return () -> {
+            post("/", hotelController::create, Role.USER);
+            get("/", hotelController::readAll);
+            get("/{id}", hotelController::read);
+            put("/{id}", hotelController::update);
+            delete("/{id}", hotelController::delete);
+
+
+        };
+    */
+            return null;
+    }
 }
