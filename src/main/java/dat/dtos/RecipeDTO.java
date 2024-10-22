42d6ccf1535aba4b80290bad9e940473fe3c17e2
peterjanas <pejanas@gmail.com>
2024-10-22 12:35:11 +0200

    dto

--- src/main/java/dat/dtos/RecipeDTO.java
+++ src/main/java/dat/dtos/RecipeDTO.java
@@ -1,5 +1,44 @@
 package dat.dtos;
 
+import dat.entities.Recipe;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Getter
+@NoArgsConstructor
 public class RecipeDTO
 {
+    private Integer id;
+
+    public RecipeDTO(Recipe recipe)
+    {
+
+    }
+
+
+    public static List<RecipeDTO> toRecipeDTOList(List<Recipe> recipes)
+    {
+        return recipes.stream().map(RecipeDTO::new).collect(Collectors.toList());
+    }
+
+    @Override
+    public boolean equals(Object o)
+    {
+        if (this == o) return true;
+        if (!(o instanceof RecipeDTO recipeDTO)) return false;
+
+        return getId().equals(recipeDTO.getId());
+    }
+
+
+    @Override
+    public int hashCode()
+    {
+        return getId().hashCode();
+    }
 }
