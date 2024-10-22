765551115b980a23045a8ccb75d17e16059c4f47
peterjanas <pejanas@gmail.com>
2024-10-22 13:30:23 +0200

    entity og dto

--- src/main/java/dat/dtos/IngredientDTO.java
+++ src/main/java/dat/dtos/IngredientDTO.java
@@ -1,5 +1,57 @@
 package dat.dtos;
 
+import dat.entities.Ingredient;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@NoArgsConstructor
+@Getter
 public class IngredientDTO
 {
+    private Integer id;
+    private String ingredientName;
+    private String amount;
+
+    public IngredientDTO(Integer id, String ingredientName, String amount)
+    {
+        this.id = id;
+        this.ingredientName = ingredientName;
+        this.amount = amount;
+    }
+
+    public IngredientDTO(Ingredient ingredient)
+    {
+        this.id = ingredient.getId();
+        this.ingredientName = ingredient.getIngredientName();
+        this.amount = ingredient.getAmount();
+    }
+
+    public static List<IngredientDTO> toIngredientDTOList(List<Ingredient> ingredients) {
+        return List.of(ingredients.stream().map(IngredientDTO::new).toArray(IngredientDTO[]::new));
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+
+        IngredientDTO that = (IngredientDTO) o;
+        return getId().equals(that.getId()) &&
+                getIngredientName().equals(that.getIngredientName()) &&
+                getAmount().equals(that.getAmount());
+    }
+
+    @Override
+    public int hashCode()
+    {
+        int result = getId().hashCode();
+        result = 31 * result + getIngredientName().hashCode();
+        result = 31 * result + getAmount().hashCode();
+        result = 31 * result + getAmount().hashCode();
+        return result;
+    }
 }
--- src/main/java/dat/entities/Recipe.java
+++ src/main/java/dat/entities/Recipe.java
@@ -1,33 +1,59 @@
 package dat.entities;
 
+import dat.dtos.RecipeDTO;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.Objects;
 import java.util.Set;
 
 @Getter
-@Setter
 @NoArgsConstructor
 @Entity
 public class Recipe
 {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "recipe_id", nullable = false, unique = true)
     private int id;
-    private String name;
+
+    @Column(name = "recipe_name", nullable = false, unique = true)
+    private String recipeName;
+
     @ManyToMany(mappedBy = "recipes", fetch = FetchType.EAGER, cascade = CascadeType.ALL)
     private Set<Ingredient> ingredients;
+
     private String servings;
 
     private String instructions;
 
-    public Recipe(String name, String servings, String instructions)
+    public Recipe(String recipeName, String servings, String instructions)
     {
-        this.name = name;
+        this.recipeName = recipeName;
         this.servings = servings;
         this.instructions = instructions;
+    }
+
+    public Recipe(RecipeDTO recipeDTO)
+    {
+        this.id = recipeDTO.getId();
+        this.recipeName = recipeDTO.getRecipeName();
+        this.servings = recipeDTO.getServings();
+        this.instructions = recipeDTO.getInstructions();
+        if (recipeDTO.getIngredients() != null) {
+            recipeDTO.getIngredients().forEach(ingredientDTO -> ingredients.add(new Ingredient(ingredientDTO)));
+        }
+    }
+    @Override
+    public boolean equals(Object o)
+    {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Recipe recipe = (Recipe) o;
+        return Objects.equals(recipeName, recipe.recipeName);
     }
 
 
