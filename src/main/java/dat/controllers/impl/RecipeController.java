eaa604d6469085cad0ae24aff77f7d15c79ec555
peterjanas <pejanas@gmail.com>
2024-10-22 12:03:15 +0200

    controller

--- src/main/java/dat/controllers/impl/RecipeController.java
+++ src/main/java/dat/controllers/impl/RecipeController.java
@@ -2,6 +2,7 @@ package dat.controllers.impl;
 
 import dat.config.HibernateConfig;
 import dat.controllers.IController;
+import dat.daos.impl.RecipeDAO;
 import dat.dtos.RecipeDTO;
 import io.javalin.http.Context;
 import jakarta.persistence.EntityManagerFactory;
@@ -10,46 +11,92 @@ import java.util.List;
 
 public class RecipeController implements IController<RecipeDTO, Integer>
 {
+    private final RecipeDAO dao;
+
+    public RecipeController()
+    {
+        EntityManagerFactory emf = HibernateConfig.getEntityManagerFactory();
+        this.dao = RecipeDAO.getInstance(emf);
+    }
 
     @Override
     public void read(Context ctx)
     {
+        // request
+        int id = ctx.pathParamAsClass("id", Integer.class).check(this::validatePrimaryKey, "Not a valid id").get();
+        // DTO
+        RecipeDTO recipeDTO = dao.read(id);
+        // response
+        ctx.res().setStatus(200);
+        ctx.json(recipeDTO, RecipeDTO.class);
 
     }
 
     @Override
     public void readAll(Context ctx)
     {
+        // List of DTOS
+        List<RecipeDTO> recipeDTOS = dao.readAll();
+        // response
+        ctx.res().setStatus(200);
+        ctx.json(recipeDTOS, RecipeDTO.class);
 
     }
 
     @Override
     public void create(Context ctx)
     {
+        // request
+        RecipeDTO jsonRequest = ctx.bodyAsClass(RecipeDTO.class);
+        // DTO
+        RecipeDTO recipeDTO = dao.create(jsonRequest);
+        // response
+        ctx.res().setStatus(201);
+        ctx.json(recipeDTO, RecipeDTO.class);
 
     }
 
     @Override
     public void update(Context ctx)
     {
+        // request
+        int id = ctx.pathParamAsClass("id", Integer.class).check(this::validatePrimaryKey, "Not a valid id").get();
+        // dto
+        RecipeDTO recipeDTO = dao.update(id, validateEntity(ctx));
+        // response
+        ctx.res().setStatus(200);
+        ctx.json(recipeDTO, RecipeDTO.class);
 
     }
 
     @Override
     public void delete(Context ctx)
     {
+        // request
+        int id = ctx.pathParamAsClass("id", Integer.class).check(this::validatePrimaryKey, "Not a valid id").get();
+        dao.delete(id);
+        // response
+        ctx.res().setStatus(204);
 
     }
 
     @Override
     public boolean validatePrimaryKey(Integer integer)
     {
-        return false;
+        return dao.validatePrimaryKey(integer);
     }
 
     @Override
     public RecipeDTO validateEntity(Context ctx)
     {
+        /*
+        return ctx.bodyValidator(HotelDTO.class)
+                .check(h -> h.getHotelAddress() != null && !h.getHotelAddress().isEmpty(), "Hotel address must be set")
+                .check(h -> h.getHotelName() != null && !h.getHotelName().isEmpty(), "Hotel name must be set")
+                .check(h -> h.getHotelType() != null, "Hotel type must be set")
+                .get();
+    */
         return null;
     }
+
 }
--- src/main/java/dat/daos/impl/RecipeDAO.java
+++ src/main/java/dat/daos/impl/RecipeDAO.java
@@ -1,5 +1,90 @@
 package dat.daos.impl;
 
-public class RecipeDAO
+import dat.daos.IDAO;
+import dat.dtos.RecipeDTO;
+import dat.entities.Recipe;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.TypedQuery;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@NoArgsConstructor(access = lombok.AccessLevel.PRIVATE)
+public class RecipeDAO implements IDAO<RecipeDTO, Integer>
 {
+    private static RecipeDAO instance;
+    private static EntityManagerFactory emf;
+
+    public static RecipeDAO getInstance(EntityManagerFactory _emf) {
+        if (instance == null) {
+            emf = _emf;
+            instance = new RecipeDAO();
+        }
+        return instance;
+    }
+
+    @Override
+    public RecipeDTO read(Integer integer)
+    {
+        try (EntityManager em = emf.createEntityManager()) {
+            Recipe recipe = em.find(Recipe.class, integer);
+            return null;
+            //return new RecipeDTO(recipe);
+        }
+    }
+
+    @Override
+    public List<RecipeDTO> readAll()
+    {
+        try (EntityManager em = emf.createEntityManager())
+        {
+           // TypedQuery<RecipeDTO> query = em.createQuery("SELECT new dat.dtos.RecipeDTO(h) FROM Recipe h", RecipeDTO.class);
+           // return query.getResultList();
+            return null;
+        }
+    }
+
+    @Override
+    public RecipeDTO create(RecipeDTO recipeDTO)
+    {
+        try (EntityManager em = emf.createEntityManager())
+        {
+            em.getTransaction().begin();
+            //Recipe recipe = new Recipe(RecipeDTO);
+            //em.persist(recipe);
+            em.getTransaction().commit();
+            //return new RecipeDTO(recipe);
+        }
+        return null;
+    }
+
+    @Override
+    public RecipeDTO update(Integer integer, RecipeDTO recipeDTO)
+    {
+        return null;
+    }
+
+    @Override
+    public void delete(Integer integer)
+    {
+        try (EntityManager em = emf.createEntityManager()) {
+            em.getTransaction().begin();
+            Recipe recipe = em.find(Recipe.class, integer);
+            if (recipe != null) {
+                em.remove(recipe);
+            }
+            em.getTransaction().commit();
+        }
+
+    }
+
+    @Override
+    public boolean validatePrimaryKey(Integer integer)
+    {
+        try (EntityManager em = emf.createEntityManager()) {
+            Recipe recipe = em.find(Recipe.class, integer);
+            return recipe != null;
+        }
+    }
 }
