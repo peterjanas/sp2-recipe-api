73db8fb68f88fa81cb236faa98c030149b291d1d
HenrikThunbo10 <henningthunbo@hotmail.com>
2024-10-23 10:40:13 +0200

    create test classes

---
+++ src/test/java/dat/IngredientDAOTest.java
@@ -0,0 +1,46 @@
+package dat;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class IngredientDAOTest
+{
+
+    @BeforeEach
+    void setUp()
+    {
+    }
+
+    @AfterEach
+    void tearDown()
+    {
+    }
+
+    @Test
+    void read()
+    {
+    }
+
+    @Test
+    void readAll()
+    {
+    }
+
+    @Test
+    void create()
+    {
+    }
+
+    @Test
+    void update()
+    {
+    }
+
+    @Test
+    void delete()
+    {
+    }
+}
