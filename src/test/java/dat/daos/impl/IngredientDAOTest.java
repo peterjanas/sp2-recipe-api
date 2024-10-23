c8bce53d52d5d4bbd21866123ffbc741aa5f64ed
peterjanas <pejanas@gmail.com>
2024-10-23 12:19:03 +0200

    tests

--- src/test/java/dat/daos/impl/IngredientDAOTest.java
+++ src/test/java/dat/daos/impl/IngredientDAOTest.java
@@ -1,4 +1,4 @@
-package dat;
+package dat.daos.impl;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
---
+++ src/test/java/dat/daos/impl/RecipeDAOTest.java
@@ -0,0 +1,74 @@
+package dat.daos.impl;
+
+import jakarta.persistence.EntityManagerFactory;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RecipeDAOTest
+{
+    private static RecipeDAO recipeDAO;
+    private static EntityManagerFactory emf;
+    private static Populator populator;
+    private static RecipeDTO r1, r2;
+    private static List<RecipeDTO> recipes;
+
+
+    @BeforeEach
+    void setUp()
+    {
+    }
+
+    @AfterEach
+    void tearDown()
+    {
+    }
+
+    @Test
+    void getInstance()
+    {
+    }
+
+
+    @Test
+    void read()
+    {
+    }
+
+    @Test
+    void readAll()
+    {
+    }
+
+    @Test
+    void readByName()
+    {
+    }
+
+    @Test
+    void readByServings()
+    {
+    }
+
+    @Test
+    void create()
+    {
+    }
+
+    @Test
+    void update()
+    {
+    }
+
+    @Test
+    void delete()
+    {
+    }
+
+    @Test
+    void validatePrimaryKey()
+    {
+    }
+}
