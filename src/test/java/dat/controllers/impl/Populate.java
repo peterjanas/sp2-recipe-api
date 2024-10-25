e8629583ea144807b80de8153da8d4dca05fb89c
peterjanas <pejanas@gmail.com>
2024-10-25 12:30:44 +0200

    populate/controller

---
+++ src/test/java/dat/controllers/impl/Populate.java
@@ -0,0 +1,124 @@
+package dat.controllers.impl;
+
+import dat.entities.Ingredient;
+import dat.entities.Recipe;
+import dat.entities.RecipeIngredient;
+import dat.security.entities.Role;
+import dat.security.entities.User;
+import dk.bugelhartmann.UserDTO;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+public class Populate
+{
+    public static UserDTO[] populateUsers(EntityManagerFactory emf)
+    {
+        User user, admin;
+        Role userRole, adminRole;
+
+        user = new User("usertest", "user123");
+        admin = new User("admintest", "admin123");
+        userRole = new Role("USER");
+        adminRole = new Role("ADMIN");
+        user.addRole(userRole);
+        admin.addRole(adminRole);
+
+        try (var em = emf.createEntityManager())
+        {
+            em.getTransaction().begin();
+            em.persist(userRole);
+            em.persist(adminRole);
+            em.persist(user);
+            em.persist(admin);
+            em.getTransaction().commit();
+        }
+        UserDTO userDTO = new UserDTO(user.getUsername(), "user123");
+        UserDTO adminDTO = new UserDTO(admin.getUsername(), "admin123");
+        return new UserDTO[]{userDTO, adminDTO};
+    }
+
+    public static Recipe[] populateData(EntityManagerFactory emf) {
+        List<Recipe> recipes = new ArrayList<>();
+
+        try (var em = emf.createEntityManager()) {
+            em.getTransaction().begin();
+
+            // Persist Ingredients first
+            Ingredient chicken = new Ingredient("Chicken");
+            Ingredient rice = new Ingredient("Rice");
+            Ingredient onion = new Ingredient("Onion");
+            Ingredient garlic = new Ingredient("Garlic");
+
+            em.persist(chicken);
+            em.persist(rice);
+            em.persist(onion);
+            em.persist(garlic);
+
+            // Create Recipes
+            Recipe chickenRice = new Recipe("Chicken and Rice", "4 servings", "Cook chicken, rice, and mix.");
+            Recipe garlicChicken = new Recipe("Garlic Chicken", "2 servings", "Cook chicken with garlic.");
+
+            // Set ingredients for each recipe
+            Set<RecipeIngredient> chickenRiceIngredients = getChickenRiceIngredients(chickenRice, chicken, rice, onion);
+            Set<RecipeIngredient> garlicChickenIngredients = getGarlicChickenIngredients(garlicChicken, chicken, garlic);
+
+            chickenRice.setIngredients(chickenRiceIngredients);
+            garlicChicken.setIngredients(garlicChickenIngredients);
+
+            // Persist Recipes
+            em.persist(chickenRice);
+            em.persist(garlicChicken);
+
+            // Add recipes to the list
+
+
+            em.getTransaction().commit();
+            return new Recipe[]{chickenRice, garlicChicken};
+        }
+    }
+
+    @NotNull
+    private static Set<RecipeIngredient> getChickenRiceIngredients(Recipe recipe, Ingredient chicken, Ingredient rice, Ingredient onion)
+    {
+        // Create RecipeIngredients for Chicken and Rice Recipe
+        RecipeIngredient chickenIngredient = new RecipeIngredient(recipe, chicken, "200g");
+        RecipeIngredient riceIngredient = new RecipeIngredient(recipe, rice, "150g");
+        RecipeIngredient onionIngredient = new RecipeIngredient(recipe, onion, "1 large");
+
+        return Set.of(chickenIngredient, riceIngredient, onionIngredient);
+    }
+
+    @NotNull
+    private static Set<RecipeIngredient> getGarlicChickenIngredients(Recipe recipe, Ingredient chicken, Ingredient garlic)
+    {
+        // Create RecipeIngredients for Garlic Chicken Recipe
+        RecipeIngredient chickenIngredient = new RecipeIngredient(recipe, chicken, "300g");
+        RecipeIngredient garlicIngredient = new RecipeIngredient(recipe, garlic, "3 cloves");
+
+        return Set.of(chickenIngredient, garlicIngredient);
+    }
+
+    public void cleanupData(EntityManagerFactory emf)
+    {
+        try (EntityManager em = emf.createEntityManager())
+        {
+            em.getTransaction().begin();
+
+            // Remove all RecipeIngredient entities
+            em.createQuery("DELETE FROM RecipeIngredient").executeUpdate();
+
+            // Remove all Recipe entities
+            em.createQuery("DELETE FROM Recipe").executeUpdate();
+
+            // Remove all Ingredient entities
+            em.createQuery("DELETE FROM Ingredient").executeUpdate();
+
+            em.getTransaction().commit();
+        }
+    }
+}
---
+++ src/test/java/dat/controllers/impl/RecipeControllerTest.java
@@ -0,0 +1,111 @@
+package dat.controllers.impl;
+
+import dat.config.ApplicationConfig;
+import dat.config.HibernateConfig;
+import dat.entities.Recipe;
+import dat.security.controllers.SecurityController;
+import dat.security.daos.SecurityDAO;
+import dat.security.exceptions.ValidationException;
+import dk.bugelhartmann.UserDTO;
+import io.javalin.Javalin;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import org.junit.jupiter.api.*;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class HotelControllerTest {
+
+    private final static EntityManagerFactory emf = HibernateConfig.getEntityManagerFactoryForTest();
+    private final static SecurityController securityController = SecurityController.getInstance();
+    private final static SecurityDAO securityDAO = new SecurityDAO(emf);
+    private static Javalin app;
+    private static Recipe[] recipes;
+    private static Recipe ChickenRice, GarlicChicken;
+    private static UserDTO userDTO, adminDTO;
+    private static String userToken, adminToken;
+    private static final String BASE_URL = "http://localhost:7007/api";
+
+    @BeforeAll
+    void setUpAll() {
+        HibernateConfig.setTest(true);
+
+        // Start api
+        app = ApplicationConfig.startServer(7070);
+    }
+
+    @BeforeEach
+    void setUp() {
+        // Populate the database with hotels and rooms
+        System.out.println("Populating database with hotels and rooms");
+        recipes = Populate.populateData(emf);
+        ChickenRice = recipes[0];
+        GarlicChicken = recipes[1];
+        UserDTO[] users = Populate.populateUsers(emf);
+        userDTO = users[0];
+        adminDTO = users[1];
+
+        try {
+            UserDTO verifiedUser = securityDAO.getVerifiedUser(userDTO.getUsername(), userDTO.getPassword());
+            UserDTO verifiedAdmin = securityDAO.getVerifiedUser(adminDTO.getUsername(), adminDTO.getPassword());
+            userToken = "Bearer " + securityController.createToken(verifiedUser);
+            adminToken = "Bearer " + securityController.createToken(verifiedAdmin);
+        }
+        catch (ValidationException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    @AfterEach
+    void tearDown() {
+        try (EntityManager em = emf.createEntityManager()){
+            em.getTransaction().begin();
+            em.createQuery("DELETE FROM User").executeUpdate();
+            em.createQuery("DELETE FROM RecipeIngredient").executeUpdate();
+            em.createQuery("DELETE FROM Recipe").executeUpdate();
+            em.getTransaction().commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @AfterAll
+    void tearDownAll() {
+        ApplicationConfig.stopServer(app);
+    }
+
+
+    @Test
+    void readAll()
+    {
+    }
+
+    @Test
+    void readByName()
+    {
+    }
+
+    @Test
+    void readByServings()
+    {
+    }
+
+    @Test
+    void create()
+    {
+    }
+
+    @Test
+    void update()
+    {
+    }
+
+    @Test
+    void delete()
+    {
+    }
+}
